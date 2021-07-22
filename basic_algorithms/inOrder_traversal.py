12b190d42565345114f0a03f56f6e6c07675c006
Anish Bangotra <73160320+AnishBangotra@users.noreply.github.com>
2021-07-22 11:58:34 +0530

    inorder traversal

---
+++ basic_algorithms/inOrder_traversal.py
@@ -0,0 +1,33 @@
+# You have a binary tree, finding inorder traversal of tree 
+# without using recursion...
+
+#Finding Inorder traversal using iterative approach(using stack)
+class Node:
+	def __init__(self, value):
+		self.data = value
+		self.left =  None
+		self.right = None
+
+def inOrder(self, root):
+	res = []
+	stack = []
+	curr = root
+	while True:
+		if curr is not None:
+			stack.append(curr)
+			curr = curr.left
+		elif stack:
+			curr = stack.pop()
+			print(curr.data, end=" ")
+			curr = curr.right
+		else:break
+	print()
+
+# Driver program
+
+root = Node(5)
+root.right = Node(3)
+root.left = Node(15)
+root.left.left = Node(11)
+root.left.right = Node(6)
+inOrder(root)
